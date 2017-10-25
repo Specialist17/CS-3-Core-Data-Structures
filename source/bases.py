f238102b5526754f525fe79fa924c8172c5114d7
Specialist17 <arocho.fernando.13@gmail.com>
2017-10-25 10:50:36 -0700

    finished decoding base requirements

--- source/bases.py
+++ source/bases.py
@@ -10,15 +10,16 @@ import pdb
 # string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
 # string.printable is digits + ascii_letters + punctuation + whitespace
-encoding_encoding_characters = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 4: "4", 5: "5", 6: "6",
+encoding_characters = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 4: "4", 5: "5", 6: "6",
               7: "7", 8: "8", 9: "9", 10: "A", 11: "B", 12: "C", 13: "D", 14: "E",
               15: "F", 16: "G", 17: "H", 18: "I", 19: "J", 20: "K", 21: "L",
               22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R", 28: "S",
               29: "T", 30: "U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z", }
-decoding_encoding_characters = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E",
-              15: "F", 16: "G", 17: "H", 18: "I", 19: "J", 20: "K", 21: "L",
-              22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R", 28: "S",
-              29: "T", 30: "U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z", }
+decoding_characters = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "4": 4, "5": 5, "6": 6,
+              "7": 7, "8": 8, "9": 9, "a": 10, "b": 11, "c": 12 , "d": 13 , "e": 14 ,
+              "f": 15 , "g": 16 , "h": 17 , "i": 18 , "j": 19 , "k": 20 , "l": 21 ,
+              "m": 22 , "n": 23 , "o": 24 , "p": 25 , "q": 26 , "r": 27 , "s": 28 ,
+              "t": 29 , "u": 30 , "v": 31 , "w": 32 , "x": 33 , "y": 34 , "z": 35 }
 
 def decode(digits, base):
     """Decode given digits in given base to number in base 10.
@@ -32,7 +33,10 @@ def decode(digits, base):
     level = 1
     total_sum = 0
     for digit in digits_list:
-        value = level * int(digit)
+        if base > 10:
+            value = level * int(decoding_characters[digit])
+        else:
+            value = level * int(digit)
         total_sum += int(value)
         level = level * base
 
@@ -233,4 +237,4 @@ if __name__ == '__main__':
     # encode_into_1010()
     decode_binary()
     decode_decimal()
-    # decode_hexadecimal()
+    decode_hexadecimal()
