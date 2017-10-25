6e9357114236c7093e988413a8f799088518325b
Specialist17 <arocho.fernando.13@gmail.com>
2017-10-25 11:14:02 -0700

    passed all tests

--- source/bases.py
+++ source/bases.py
@@ -77,13 +77,12 @@ def convert(digits, base1, base2):
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
     # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
+
+    if base1 == 10:
+        return encode(int(digits), base2)
+    else:
+        base_10_repr = decode(digits, base1)
+        return encode(base_10_repr, base2)
 
 
 def main():
@@ -228,13 +227,13 @@ def decode_hexadecimal():
     print(decode('f007ba11', 16) == 4027038225)
 
 if __name__ == '__main__':
-    # main()
+    main()
     # encode_binary()
     # encode_hexadecimal()
     # encode_1234()
     # encode_248975()
     # encode_into_10()
     # encode_into_1010()
-    decode_binary()
-    decode_decimal()
-    decode_hexadecimal()
+    # decode_binary()
+    # decode_decimal()
+    # decode_hexadecimal()
