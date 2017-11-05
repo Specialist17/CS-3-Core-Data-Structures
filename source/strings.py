eca5bf0c4266f9b1c6ca1377b2aa95954a2f7a20
Specialist17 <arocho.fernando.13@gmail.com>
2017-11-04 19:13:10 -0700

    annotations for string algorithms

--- source/strings.py
+++ source/strings.py
@@ -2,6 +2,8 @@
 
 def contains(text, pattern):
     """Return a boolean indicating whether pattern occurs in text."""
+    """ O(1) best case, O(n*m) worst case
+        n = length of text, m = length of pattern"""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
@@ -16,21 +18,13 @@ def contains(text, pattern):
 def find_index(text, pattern):
     """Return the starting index of the first occurrence of pattern in text,
     or None if not found."""
+    """ O(1) best case, O(n*m) worst case
+        n = length of text, m = length of pattern"""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
     if pattern == "":
         return 0
-
-    # for index, char in enumerate(text):
-    #     count = 0
-    #     for index2, pattern_char in enumerate(pattern):
-    #         if text[index+index2] == pattern[index2]:
-    #             count += 1
-    #         else:
-    #             break
-    #     if count == len(pattern):
-    #         return index
 
     all_indexes = find_all_indexes(text, pattern)
     if all_indexes == []:
@@ -39,34 +33,36 @@ def find_index(text, pattern):
         return all_indexes[0]
 
 
-
 def find_all_indexes(text, pattern):
     """Return a list of starting indexes of all occurrences of pattern in text,
     or an empty list if not found."""
+    """ O(n) best case, O(n*m) worst case
+        n = length of text, m = length of pattern"""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
 
-    index_list = []
-    if pattern == "":
-        for index in range(len(text)):
-            index_list.append(index)
-        return index_list
+    index_list = []                                         # O(1), create list
+    if pattern == "":                                       # check if patter is empty string, O(1) because it's an empty string
+        for index in range(len(text)):                      # iterate through the whole text, because everything counts
+                                                            # as a pattern, 0(n), n = length of text
+            index_list.append(index)                        # append index, O(1) in average
+        return index_list                                   # return, O(1)
 
     # index = 0
-    for index, char in enumerate(text):
-        count = 0
-        for index2, pattern_char in enumerate(pattern):
-            if index + index2 < (len(text)):
-                if text[index+index2] == pattern[index2]:
-                    count += 1
+    for index, char in enumerate(text):                     # iterate through whole text, O(n), n = length of text
+        count = 0                                           # create variable, O(1)
+        for index2, pattern_char in enumerate(pattern):     # iterate through the pattern, O(m) worst case
+            if index + index2 < (len(text)):                # compare a sum O(1) to a length O(1), O(1)
+                if text[index+index2] == pattern[index2]:   # check if text index is equal to pattern index, O(1)
+                    count += 1                              # add to count, O(1)
                     continue
                 else:
                     break
-        if count == len(pattern):
-            index_list.append(index)
+        if count == len(pattern):                           # check if count is the same as pttern length, O(1)
+            index_list.append(index)                        # append to index_list, O(1) in average
 
-    return index_list
+    return index_list                                       # return list, O(1)
 
 
 def test_string_algorithms(text, pattern):
