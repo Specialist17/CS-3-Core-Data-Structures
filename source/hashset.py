d2f373cc72808270d91afe8ea9eab3f9deb9813b
Specialist17 <arocho.fernando.13@gmail.com>
2017-11-09 18:51:55 -0800

    created tests for Set

--- source/hashset.py
+++ source/hashset.py
@@ -22,14 +22,18 @@ class Set(object):
         """Return a string representation of this Set"""
         return 'Set({!r})'.format(self.table.keys())
 
+    def is_empty(self):
+        return self.size == 0
+
     def contains(self, element):
         """Return True if this Set contains the given element (key), or False."""
         return self.table.contains(element)
 
     def add(self, element):
         """Add element to this set, if not present already"""
-        self.table.set(element, 0)
-        self.size += 1
+        if not self.contains(element):
+            self.table.set(element, 0)
+            self.size += 1
 
     def remove(self, element):
         """Remove the given element, or raise KeyError"""
@@ -39,34 +43,43 @@ class Set(object):
     def union(self, other_set):
         union_set = Set()
         union_set.table = self.table
-        for element in other_set:
+        for element in other_set.table.keys():
             union_set.table.set(element, 0)
-        return union_set.keys()
+        return union_set.table.keys()
 
     def intersection(self, other_set):
         intersection_set = Set()
 
-        for element in other_set:
+        for element in other_set.table.keys():
             if self.table.contains(element):
-                intersection_set.table.set(element)
+                intersection_set.table.set(element, 0)
 
-        return intersection_set.keys()
+        return intersection_set.table.keys()
 
     def difference(self, other_set):
         difference_set = Set()
 
         for element in self.table.keys():
-             if element not in other_set:
-                 difference_set.table.set(element)
+             if other_set.table.contains(element):
+                 difference_set.table.set(element, 0)
 
-        return difference_set
+        return difference_set.table.keys()
 
     def is_subset(self, other_set):
 
         for element in self.table.keys():
-            if element in other_set:
+            if element in other_set.table.keys():
                 continue
             else:
                 return False
 
         return True
+
+
+def test_set():
+    s = Set(['A', 'B', 'C', 'D', 'X'])
+    print(s)
+    print(repr(s))
+
+if __name__ == "__main__":
+    test_set()
--- source/set_test.py
+++ source/set_test.py
@@ -8,61 +8,165 @@ class SetTest(unittest.TestCase):
 
     def test_init(self):
         s = Set()
+        assert s.size == 0
+        assert s.table.length() == 0
 
     def test_init_with_list(self):
-        s = Stack(['A', 'B', 'C'])
-        assert s.peek() == 'C'
-        assert s.length() == 3
+        s = Set(['A', 'B', 'C'])
+        assert s.size == 3
+        assert s.table.length() == 3
         assert s.is_empty() is False
 
-    def test_length(self):
-        s = Stack()
-        assert s.length() == 0
-        s.push('A')
-        assert s.length() == 1
-        s.push('B')
-        assert s.length() == 2
-        s.pop()
-        assert s.length() == 1
-        s.pop()
-        assert s.length() == 0
+    def test_contains(self):
+        s = Set(['A', 'B', 'C', 'D', 'X'])
+        assert s.contains('C') == True
+        assert s.contains('A') == True
 
-    def test_push(self):
-        s = Stack()
-        s.push('A')
-        assert s.peek() == 'A'
-        assert s.length() == 1
-        s.push('B')
-        assert s.peek() == 'B'
-        assert s.length() == 2
-        s.push('C')
-        assert s.peek() == 'C'
-        assert s.length() == 3
-        assert s.is_empty() is False
+        s.add('Y')
+        assert s.contains('Y') == True
+        assert s.contains('Z') == False
 
-    def test_peek(self):
-        s = Stack()
-        assert s.peek() is None
-        s.push('A')
-        assert s.peek() == 'A'
-        s.push('B')
-        assert s.peek() == 'B'
-        s.pop()
-        assert s.peek() == 'A'
-        s.pop()
-        assert s.peek() is None
+        s.add('Z')
+        assert s.contains('Z') == True
 
-    def test_pop(self):
-        s = Stack(['A', 'B', 'C'])
-        assert s.pop() == 'C'
-        assert s.length() == 2
-        assert s.pop() == 'B'
-        assert s.length() == 1
-        assert s.pop() == 'A'
-        assert s.length() == 0
-        assert s.is_empty() is True
-        with self.assertRaises(ValueError):
-            s.pop()
+        s.remove('Y')
+        assert s.contains('Y') == False
+
+    def test_add(self):
+        s = Set()
+        assert s.size == 0
+
+        s.add('Y')
+        assert s.contains('Y') == True
+        assert s.size == 1
+
+        s.add('Y')
+        assert s.size == 1
+
+        assert s.contains('Z') == False
+        s.add('Z')
+        assert s.contains('Z') == True
+        assert s.size == 2
+
+    def test_remove(self):
+        s = Set(['A', 'B', 'C', 'D', 'X'])
+        assert s.size == 5
+
+        s.remove('A')
+        assert s.contains('A') == False
+        assert s.size == 4
+
+        with self.assertRaises(KeyError):
+            s.remove('Z')  # item not in set
+
+        assert s.size == 4
+
+        s.remove('B')
+        assert s.contains('B') == False
+        assert s.size == 3
+
+        s.remove('C')
+        assert s.size == 2
+        s.remove('D')
+        assert s.size == 1
+        s.remove('X')
+        assert s.size == 0
+
+    def test_union(self):
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['M', 'N', 'O', 'A', 'P'])
+        assert sA.union(sB).sort() == ['A', 'B', 'C', 'D', 'X', 'M', 'N', 'O', 'P'].sort()
+        assert sB.union(sA).sort() == ['A', 'B', 'C', 'D', 'X', 'M', 'N', 'O', 'P'].sort()
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set()
+        assert sA.union(sB).sort() == ['A', 'B', 'C', 'D', 'X'].sort()
+        assert sB.union(sA).sort() == ['A', 'B', 'C', 'D', 'X'].sort()
+
+        sA = Set()
+        sB = Set()
+        assert sA.union(sB) == []
+        assert sB.union(sA) == []
+
+    def test_intersection(self):
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['A', 'B', 'C', 'D', 'X'])
+        assert sA.intersection(sB).sort() == ['A', 'B', 'C', 'D', 'X'].sort()
+        assert sB.intersection(sA).sort() == ['A', 'B', 'C', 'D', 'X'].sort()
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['A', 'B', 'M', 'P', 'Z'])
+        assert sA.intersection(sB).sort() == ['A', 'B', 'C', 'D', 'X'].sort()
+        assert sB.intersection(sA).sort() == ['A', 'B'].sort()
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['M', 'N', 'O', 'A', 'P'])
+        assert sA.intersection(sB).sort() == ['A'].sort()
+        assert sB.intersection(sA).sort() == ['A'].sort()
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set()
+        assert sA.intersection(sB).sort() == [].sort()
+        assert sB.intersection(sA).sort() == [].sort()
+        #
+        sA = Set()
+        sB = Set()
+        assert sA.intersection(sB) == []
+        assert sB.intersection(sA) == []
+
+    def test_difference(self):
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['A', 'B', 'C', 'D', 'X'])
+        assert sA.difference(sB).sort() == [].sort()
+        assert sB.difference(sA).sort() == [].sort()
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['A', 'B', 'M', 'P', 'Z'])
+        assert sA.difference(sB).sort() == ['C', 'D', 'X'].sort()
+        assert sB.difference(sA).sort() == ['M', 'P', 'Z'].sort()
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['M', 'N', 'O', 'A', 'P'])
+        assert sA.difference(sB).sort() == ['D'].sort()
+        assert sB.difference(sA).sort() == ['M', 'N', 'O', 'P'].sort()
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set()
+        assert sA.difference(sB).sort() == ['A', 'B', 'C', 'D', 'X'].sort()
+        assert sB.difference(sA).sort() == [].sort()
+
+        sA = Set()
+        sB = Set()
+        assert sA.difference(sB) == []
+        assert sB.difference(sA) == []
+
+    def test_is_subset(self):
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['A', 'B', 'C', 'D', 'X'])
+        assert sA.is_subset(sB) == True
+        assert sB.is_subset(sA) == True
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['A', 'B'])
+        assert sA.is_subset(sB) == False
+        assert sB.is_subset(sA) == True
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set(['A', 'B', 'M'])
+        assert sA.is_subset(sB) == False
+        assert sB.is_subset(sA) == False
+
+        sA = Set(['A', 'B', 'C', 'D', 'X'])
+        sB = Set()
+        assert sA.is_subset(sB) == False
+        assert sB.is_subset(sA) == True
+
+        sA = Set()
+        sB = Set()
+        assert sA.is_subset(sB) == True
+        assert sB.is_subset(sA) == True
+
+
 
 
 if __name__ == '__main__':
